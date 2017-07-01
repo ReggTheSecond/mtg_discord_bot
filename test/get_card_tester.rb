f6e8e5b6ccab9700674460f6c28ad76b6ad37592
Simon Murphy <murphysimon@outlook.com>
2017-07-01 21:39:06 +0100

    Added tests for get_card_link() and get_nickname().

--- test/get_card_tester.rb
+++ test/get_card_tester.rb
@@ -2,8 +2,16 @@ require "test/unit"
 require_relative '../src/whats_the_pick.rb'
 require_relative '../src/get_card.rb'
 
-class Card_SearcherTester < Test::Unit::TestCase
+class Card_Searcher_Tester < Test::Unit::TestCase
   def test_get_card_name()
     assert_equal("Goblin Rabblemaster", Card_Searcher.new().get_card_name("Goblin Rabblemaster"))
+  end
+
+  def test_get_card_link()
+    assert_equal("http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=383258&type=card", Card_Searcher.new().get_card_link("Goblin Rabblemaster"))
+  end
+
+  def test_get_nickname()
+    assert_equal("Dig Through TIme", Card_Searcher.new().get_nickname("Dig"))
   end
 end
