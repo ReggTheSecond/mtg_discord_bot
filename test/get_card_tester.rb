4b1fabb93aae9a4627cc96d7829926b238f48baf
Simon Murphy <simonmurphy@bluebridgetech.com>
2017-07-07 16:59:50 +0100

    Updated get_card.rb to reuse more code and repeat itself less often. Added tests for get_card_link(), get_split_card(), get_specific_set(). Updated other tests to test new methods of getting cards from gatherer.

--- test/get_card_tester.rb
+++ test/get_card_tester.rb
@@ -11,7 +11,15 @@ class Card_Searcher_Tester < Test::Unit::TestCase
     assert_equal("http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=383258&type=card", Card_Searcher.new().get_card_link("Goblin Rabblemaster"))
   end
 
+  def test_get_split_card()
+    assert_equal("http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=107423&type=card", Card_Searcher.new().get_split_card("rise//fall"))
+  end
+
+  def test_get_specific_set()
+    assert_equal("http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=202687&type=card", Card_Searcher.new().get_specific_set("blistergrub", "SOM"))
+  end
+
   def test_get_nickname()
-    assert_equal("dig Through time", Card_Searcher.new().get_nickname("Dig"))
+    assert_equal("dig through time", Card_Searcher.new().get_nickname("dig"))
   end
 end
